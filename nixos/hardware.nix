25e6ff017e152326a2e3daacaa5e7a38a3eabe2e
Sylvie Hacker <sylvie@karpador.xyz>
2023-11-25 13:38:44 +0100

    add workaround for zfs silent data corruption bug (yay…)

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -16,6 +16,10 @@
     extraModulePackages = [ ];
 
     kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+    # https://www.reddit.com/r/zfs/comments/1826lgs/psa_its_not_block_cloning_its_a_data_corruption/
+    # https://github.com/openzfs/zfs/issues/15526#issuecomment-1823737998
+    kernelParams = [ "zfs.zfs_dmu_offset_next_sync=0" ];
   };
 
 
