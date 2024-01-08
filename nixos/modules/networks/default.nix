471c3ca15d70f337a30fbff0cff54295f0f4ac9f
Sylvie Hacker <sylvie@karpador.xyz>
2024-01-08 16:28:50 +0100

    adjust hostname and hostid to tzuyu

--- nixos/modules/networks/default.nix
+++ nixos/modules/networks/default.nix
@@ -2,22 +2,14 @@
 {
   # part of me really wants to keep these in configuration.nix
   # hostId could even go into hardware.nix cause it's a zfs thing
-  networking.hostName = "godemiche";
-  networking.hostId = "62323837";
+  networking.hostName = "tzuyu";
+  networking.hostId = "56a91f16";
 
   # I do use this on desktop
   networking.networkmanager.enable = true;
 
   networking.firewall = {
     enable = true;
-    # allow all traffic from zerotier
-    trustedInterfaces = ["ztosikmzde"];
-  };
-
-  # VPN - ZeroTier
-  services.zerotierone = {
-    enable = true;
-    joinNetworks = ["48d6023c46a45822"];
   };
 
   # VPN - Tailscale
