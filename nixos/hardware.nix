a88a53e4c1c317b44990f0046df3a08be09ed52f
Sylvie Hacker <sylvie@karpador.xyz>
2024-01-08 16:42:24 +0100

    remove some unneeded things

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -8,7 +8,7 @@
     };
     
     initrd = {
-      availableKernelModules = [ "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sdhci_acpi" ];
+      availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "sdhci_pci" ];
       kernelModules = [ ];
     };
 
@@ -23,28 +23,15 @@
 
 
   fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-uuid/5ef48d0e-0e07-4bd2-84a8-0ca924789c0e";
-      fsType = "ext4";
-    };
-    "/nix" = {
-      device = "zroot/ROOT/nixos/nix";
-      fsType = "zfs";
-      neededForBoot = true;
-    };
-    "/boot" = { 
-      device = "/dev/disk/by-uuid/A215-063B";
-      fsType = "vfat";
-    };
+    
   };
-  swapDevices = [{device = "/dev/disk/by-uuid/7d7734a2-ecec-4bc2-a8bd-784d3038c713";}];
+  swapDevices = [{device = "/dev/disk/by-uuid/";}];
 
-  hardware.bluetooth.enable = false;
+  hardware.bluetooth.enable = true;
   networking.useDHCP = lib.mkDefault true;
 
   hardware.enableRedistributableFirmware = true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "ondemand";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
