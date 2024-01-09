95fffa35dde2cf7c93447a3baa572ba81241b550
Sylvie Hacker <sylvie@karpador.xyz>
2024-01-09 13:40:37 +0100

    start moving GNOME options into home manager config

--- home-manager/modules/desktop/gnome/default.nix
+++ home-manager/modules/desktop/gnome/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   imports = [
     ./apps.nix
@@ -8,4 +8,19 @@
   home.packages = with pkgs.gnome; [
     gnome-tweaks
   ];
+
+  # gonna put the settings here for now, mayb move to submodules later
+  dconf.settings = {
+    # interface preferences
+    "org/gnome/desktop/interface".show-battery-percentage = true;
+    # power stuff
+    "org/gnome/settings-daemon/plugins/color".night-light-enabled = true;
+    "org/gnome/desktop/session".idle-delay = lib.hm.gvariant.mkUint32 0;
+    "org/gnome/settings-daemon/plugins/power" = {
+      power-saver-profile-on-low-battery = true;
+      sleep-inactive-ac-type = "nothing";
+      sleep-inactive-battery-type = "suspend";
+      sleep-inactive-battery-timeout = 1800;
+    };
+  };
 }
