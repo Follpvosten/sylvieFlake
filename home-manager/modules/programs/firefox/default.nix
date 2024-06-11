588e7636037cb0076122821c39c3512083f12cf6
Sylvie Hacker <sylvie@karpador.xyz>
2024-06-11 21:56:29 +0200

    initial declarative firefox config

--- home-manager/modules/programs/firefox/default.nix
+++ home-manager/modules/programs/firefox/default.nix
@@ -1,7 +1,19 @@
 { ... }:
 {
-  programs.firefox = {
-    enable = true;
-    # TODO: declarative config
+  programs.firefox.enable = true;
+  programs.firefox.profiles."default-sylvie" = {
+    id = 0;
+    name = "default";
+    path = "lu044f5h.default";
+    search.order = [ "DuckDuckGo" ];
+    search.force = true;
+    settings = {
+      "browser.startup.homepage" = "https://start.duckduckgo.com";
+      "browser.newtabpage.enabled" = false;
+      "browser.aboutConfig.showWarning" = false;
+      "browser.search.suggest.enabled" = false;
+      "browser.urlbar.suggest.searches" = false;
+      "browser.tabs.insertAfterCurrent" = true;
+    };
   };
 }
