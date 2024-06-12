67a38296b2e9103398480954cf504a0c0b350629
Sylvie Hacker <sylvie@karpador.xyz>
2024-06-12 16:06:32 +0200

    firefox: inline UI customisation

--- home-manager/modules/programs/firefox/default.nix
+++ home-manager/modules/programs/firefox/default.nix
@@ -22,6 +22,50 @@
       "extensions.pocket.enabled" = false;
       "identity.fxaccounts.enabled" = false;
       "identity.fxaccounts.toolbar.enabled" = false;
+      "browser.uiCustomization.state" = builtins.toJSON {
+        "placements" = {
+          "widget-overflow-fixed-list" = [];
+          "unified-extensions-area" = [];
+          "nav-bar" = [
+              "back-button"
+              "forward-button"
+              "stop-reload-button"
+              "home-button"
+              "urlbar-container"
+              "downloads-button"
+              "unified-extensions-button"
+              "ublock0_raymondhill_net-browser-action"
+              # why is bitwarden called that???
+              "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+          ];
+          "toolbar-menubar" = [
+              "menubar-items"
+          ];
+          "TabsToolbar" = [
+              "tabbrowser-tabs"
+              "new-tab-button"
+              "alltabs-button"
+          ];
+          "PersonalToolbar" = [
+              "import-button"
+              "personal-bookmarks"
+          ];
+        };
+        "seen" = [
+            "developer-button"
+            "ublock0_raymondhill_net-browser-action"
+            "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
+        ];
+        "dirtyAreaCache" = [
+            "nav-bar"
+            "PersonalToolbar"
+            "toolbar-menubar"
+            "TabsToolbar"
+            "unified-extensions-area"
+        ];
+        "currentVersion" = 20;
+        "newElementCount" = 3;
+      };
     };
     extensions = with config.nur.repos.rycee.firefox-addons; [
       ublock-origin
