f9997c3bbcf373738e0e1e58ad7c4f8e147a79d9
Sylvie Hacker <sylvie@karpador.xyz>
2024-06-16 10:13:22 +0200

    firefox: add simple-tab-groups, move ui config to separate file

--- home-manager/modules/programs/firefox/default.nix
+++ home-manager/modules/programs/firefox/default.nix
@@ -27,55 +27,13 @@
       "intl.locale.requested" = "de,en-US";
       "intl.regional_prefs.use_os_locales" = true;
       "datareporting.healthreport.uploadEnabled" = false;
-      "browser.uiCustomization.state" = builtins.toJSON {
-        "placements" = {
-          "widget-overflow-fixed-list" = [];
-          "unified-extensions-area" = [];
-          "nav-bar" = [
-              "back-button"
-              "forward-button"
-              "stop-reload-button"
-              "home-button"
-              "urlbar-container"
-              "downloads-button"
-              "ublock0_raymondhill_net-browser-action"
-              # why is bitwarden called that???
-              "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
-              "unified-extensions-button"
-          ];
-          "toolbar-menubar" = [
-              "menubar-items"
-          ];
-          "TabsToolbar" = [
-              "tabbrowser-tabs"
-              "new-tab-button"
-              "alltabs-button"
-          ];
-          "PersonalToolbar" = [
-              "import-button"
-              "personal-bookmarks"
-          ];
-        };
-        "seen" = [
-            "developer-button"
-            "ublock0_raymondhill_net-browser-action"
-            "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action"
-        ];
-        "dirtyAreaCache" = [
-            "nav-bar"
-            "PersonalToolbar"
-            "toolbar-menubar"
-            "TabsToolbar"
-            "unified-extensions-area"
-        ];
-        "currentVersion" = 20;
-        "newElementCount" = 3;
-      };
+      "browser.uiCustomization.state" = builtins.toJSON (import ./uiconfig.nix);
     };
     extensions = with config.nur.repos.rycee.firefox-addons; [
       ublock-origin
       bitwarden
       consent-o-matic
+      simple-tab-groups
     ];
   };
 }
