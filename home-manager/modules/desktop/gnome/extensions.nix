489399551bac857f494218b0fecafb699ebccdd5
Sylvie Hacker <sylvie@karpador.xyz>
2024-07-19 09:14:28 +0200

    gnome: enable and configure night-theme-switcher extension

--- home-manager/modules/desktop/gnome/extensions.nix
+++ home-manager/modules/desktop/gnome/extensions.nix
@@ -3,5 +3,14 @@
   programs.gnome-shell.enable = true;
   programs.gnome-shell.extensions = with pkgs.gnomeExtensions; [
     { package = appindicator; }
+    { package = night-theme-switcher; }
   ];
+  dconf.settings = {
+    "org/gnome/shell/extensions/nightthemeswitcher/time" = {
+      # it complains without this
+      manual-schedule = true;
+      sunrise = 7.5;
+      sunset = 20.0;
+    };
+  };
 }
