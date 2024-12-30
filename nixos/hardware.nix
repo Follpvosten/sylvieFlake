2e15aafa3a8fd141330be70779b17e6413bc430d
Sylvie Hacker <sylvie@karpador.xyz>
2024-12-30 08:29:06 +0100

    zfs: reduce arc max further

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -15,7 +15,7 @@
 
     kernelModules = [ ];
     kernelParams = [
-      "zfs.zfs_arc_max=536870912"
+      "zfs.zfs_arc_max=134217728"
       "video=DSI-1:panel_orientation=right_side_up"
     ];
   };
