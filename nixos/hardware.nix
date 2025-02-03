dd41db4f9ea0ddf4984d3fd1ddcb762dd358bccc
Sylvie Hacker <sylvie@karpador.xyz>
2025-02-03 08:30:57 +0100

    bring zfs kernel params up to date

--- nixos/hardware.nix
+++ nixos/hardware.nix
@@ -16,12 +16,7 @@
     kernelModules = [ "kvm-amd" ];
     extraModulePackages = [ ];
 
-    # https://www.reddit.com/r/zfs/comments/1826lgs/psa_its_not_block_cloning_its_a_data_corruption/
-    # https://github.com/openzfs/zfs/issues/15526#issuecomment-1823737998
-    kernelParams = [
-      "zfs.zfs_dmu_offset_next_sync=0"
-      "zfs.zfs_arc_max=12884901888"
-    ];
+    kernelParams = [ "zfs.zfs_arc_max=524288000" ];
   };
 
 
