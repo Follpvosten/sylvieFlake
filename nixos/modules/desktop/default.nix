c4bc795884ef958224c6ea617b9df93b353b32e9
Sylvie Hacker <sylvie@karpador.xyz>
2025-02-05 19:30:19 +0100

    move xserver settings out of kde.nix

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -1,12 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  # and i mean *way* better.
   imports = [
     ./kde.nix
     ./sound.nix
   ];
 
   programs.steam.enable = true;
+
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "us";
+      variant = "de_se_fi";
+      # unset to make ctrl-backspace work in gtk apps
+      options = "";
+    };
+    excludePackages = [ pkgs.xterm ];
+  };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
--- nixos/modules/desktop/kde.nix
+++ nixos/modules/desktop/kde.nix
@@ -1,12 +1,5 @@
 { ... }:
 {
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "us";
-      variant = "de_se_fi";
-    };
-  };
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm.enable = true;
 
