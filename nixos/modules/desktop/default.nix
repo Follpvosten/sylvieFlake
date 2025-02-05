af79cea84741c9e7a3f0066a2bffb9192d7ee51c
Sylvie Hacker <sylvie@karpador.xyz>
2025-02-05 19:30:28 +0100

    enable ananicy

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -19,4 +19,29 @@
   };
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  services.ananicy = {
+    enable = true;
+    package = pkgs.ananicy-cpp;
+    rulesProvider = pkgs.ananicy-cpp;
+    extraTypes = [
+      { type = "desktop"; nice = -10; }
+      { type = "gui-app"; nice = -15; }
+      { type = "call-app"; nice = -20; }
+    ];
+    extraRules = let
+      desktopProcesses = [
+        "gnome-shell" "Xwayland" "mutter-x11-frames" "rtkit-daemon" "gdm"
+        "gnome-session-binary"
+      ];
+      guiApps = [
+        "nautilus" "totem" "geary" "gnome-calendar" "gnome-clocks" "diebahn"
+        "snapshot" "firefox" "codium" "fractal" "signal-desktop" "telegram-desktop"
+        "dev.geopjr.Tuba" ".gnome-terminal" "gnome-terminal-server"
+      ];
+    in
+      map (x: { name = x; type = "desktop"; }) desktopProcesses
+      ++ map (x: { name = x; type = "gui-app"; }) guiApps
+      ++ [{ name = ".Discord-wrapped"; type = "call-app"; }];
+  };
 }
