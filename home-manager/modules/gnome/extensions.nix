def01b4bd0878b6bfd518e47301b30d4d5218a26
Sylvie Hacker <sylvie@karpador.xyz>
2025-02-08 10:16:22 +0100

    configure auto-power-profile gnome extension

--- home-manager/modules/gnome/extensions.nix
+++ home-manager/modules/gnome/extensions.nix
@@ -4,6 +4,7 @@
   programs.gnome-shell.extensions = with pkgs.gnomeExtensions; [
     { package = appindicator; }
     { package = night-theme-switcher; }
+    { package = auto-power-profile; }
   ];
   dconf.settings = {
     "org/gnome/shell/extensions/nightthemeswitcher/time" = {
@@ -11,6 +12,15 @@
       manual-schedule = true;
       sunrise = 8.0;
       sunset = 16.5;
+    };
+    "org/gnome/shell/extensions/auto-power-profile" = {
+      # profile on battery should always be power saver
+      bat = "power-saver";
+      # avoid annoying noise when charging
+      ac = "balanced";
+      # if the profile is manually overridden,
+      # switch to power saver at XX% battery
+      threshold = 30;
     };
   };
 }
