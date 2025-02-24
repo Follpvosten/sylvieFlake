5b2947dab711bbaa65c35bb04a1399101320e6cb
Sylvie Hacker <sylvie@karpador.xyz>
2025-02-24 13:41:34 +0100

    update vscode config

--- home-manager/modules/programs/vscode/default.nix
+++ home-manager/modules/programs/vscode/default.nix
@@ -3,43 +3,46 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-
-    extensions = with pkgs.vscode-extensions; [
-      # General
-      usernamehw.errorlens ms-azuretools.vscode-docker 
-      matthewpi.caddyfile-support mkhl.direnv
-      # 🦀
-      vadimcn.vscode-lldb rust-lang.rust-analyzer
-      bungcip.better-toml serayuzgur.crates
-      # ❄️
-      jnoortheen.nix-ide
-      # terraform
-      hashicorp.terraform
-    ];
     
-    enableExtensionUpdateCheck = false;
-    enableUpdateCheck = false;
     mutableExtensionsDir = false;
-    userSettings = {
-      # basics
-      "files.insertFinalNewline" = true;
-      "editor.formatOnSave" = true;
-      "update.mode" = "none";
-      "window.titleBarStyle" = "custom";
-      "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monospace', monospace";
-      "git.openRepositoryInParentFolders" = "never";
-      "window.autoDetectColorScheme" = true;
-      "workbench.editor.enablePreview" = false;
-      "chat.commandCenter.enabled" = false;
-      # 🦀
-      "rust-analyzer.cargo.features" = "all";
-      "rust-analyzer.check.command" = "clippy";
-      # ❄️
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "nil";
-      # TODO: I hate this
-      "nix.serverSettings" = {
-        nil.formatting.command = ["cat"];
+
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        # General
+        usernamehw.errorlens ms-azuretools.vscode-docker 
+        matthewpi.caddyfile-support mkhl.direnv
+        # 🦀
+        vadimcn.vscode-lldb rust-lang.rust-analyzer
+        bungcip.better-toml serayuzgur.crates
+        # ❄️
+        jnoortheen.nix-ide
+        # terraform
+        hashicorp.terraform
+      ];
+      
+      enableExtensionUpdateCheck = false;
+      enableUpdateCheck = false;
+      userSettings = {
+        # basics
+        "files.insertFinalNewline" = true;
+        "editor.formatOnSave" = true;
+        "update.mode" = "none";
+        "window.titleBarStyle" = "custom";
+        "editor.fontFamily" = "'FiraCode Nerd Font Mono', 'Droid Sans Mono', 'monospace', monospace";
+        "git.openRepositoryInParentFolders" = "never";
+        "window.autoDetectColorScheme" = true;
+        "workbench.editor.enablePreview" = false;
+        "chat.commandCenter.enabled" = false;
+        # 🦀
+        "rust-analyzer.cargo.features" = "all";
+        "rust-analyzer.check.command" = "clippy";
+        # ❄️
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nil";
+        # TODO: I hate this
+        "nix.serverSettings" = {
+          nil.formatting.command = ["cat"];
+        };
       };
     };
   };
