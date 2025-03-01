b267befc722ba4aa6f3a030526d080cf259a63ef
Sylvie Hacker <sylvie@karpador.xyz>
2025-03-01 18:54:33 +0100

    ananicy: add niced up compiler category

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -24,6 +24,7 @@
     package = pkgs.ananicy-cpp;
     rulesProvider = pkgs.ananicy-cpp;
     extraTypes = [
+      { type = "compiler"; nice = 10; }
       { type = "desktop"; nice = -10; }
       { type = "gui-app"; nice = -15; }
       { type = "call-app"; nice = -20; }
@@ -38,9 +39,11 @@
         "snapshot" "firefox" "codium" "fractal" "signal-desktop" "telegram-desktop"
         "dev.geopjr.Tuba" ".gnome-terminal" "gnome-terminal-server"
       ];
+      compilers = [ "nix" "rustc" ];
     in
       map (x: { name = x; type = "desktop"; }) desktopProcesses
       ++ map (x: { name = x; type = "gui-app"; }) guiApps
+      ++ map (x: { name = x; type = "compiler"; }) compilers
       ++ [{ name = ".Discord-wrapped"; type = "call-app"; }];
   };
 }
