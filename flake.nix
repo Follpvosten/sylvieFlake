4bf73e9d5abc46db0e7bdf6ab09cbbb392298ee6
Sylvie Hacker <sylvie@karpador.xyz>
2025-05-30 08:02:01 +0200

    remove lix

--- flake.nix
+++ flake.nix
@@ -3,27 +3,22 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0-3.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, lix-module, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       godemiche = nixpkgs.lib.nixosSystem { 
         system = "x86_64-linux";
         modules = [
-          lix-module.nixosModules.default
           ./nixos/configuration.nix
 
           home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = !false;
+            home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.sylvie = import home-manager/sylvie.nix;
           }
