109262dd639cde08d51c4c44012676e5aa0eae1c
Sylvie Hacker <sylvie@karpador.xyz>
2025-06-15 20:43:37 +0200

    vscode: add ron extension

--- home-manager/modules/programs/vscode/default.nix
+++ home-manager/modules/programs/vscode/default.nix
@@ -14,6 +14,7 @@
         # 🦀
         vadimcn.vscode-lldb rust-lang.rust-analyzer
         tamasfe.even-better-toml fill-labs.dependi
+        a5huynh.vscode-ron
         # ❄️
         jnoortheen.nix-ide
         # terraform
