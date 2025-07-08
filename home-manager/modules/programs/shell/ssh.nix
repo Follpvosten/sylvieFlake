92c5c85605b37fef4d4fcadc423fb73f637a068f
Sylvie Hacker <sylvie@karpador.xyz>
2025-07-08 11:16:58 +0200

    remove no longer needed ssh config entries

--- home-manager/modules/programs/shell/ssh.nix
+++ home-manager/modules/programs/shell/ssh.nix
@@ -15,23 +15,6 @@
         hostname = "130.255.78.72";
         port = 22666;
       };
-      "mail" = {
-        user = "jonas";
-        hostname = "mail.karpador.xyz";
-        port = 45822;
-      };
-      "religio" = {
-        user = "jonas";
-        hostname = "192.99.161.36";
-      };
-      "dgnfog-netcup" = {
-        user = "jonasrinner";
-        hostname = "172.16.5.1";
-      };
-      "ci-runner" = {
-        user = "jonas";
-        hostname = "ci-runner01.dungeonfog.com";
-      };
     };
   };
 }
