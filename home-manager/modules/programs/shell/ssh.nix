e400f6bf48157d005700885fc452f6b936d8212b
Sylvie Hacker <sylvie@karpador.xyz>
2025-07-08 11:21:58 +0200

    ssh config: remove old kcloud host

--- home-manager/modules/programs/shell/ssh.nix
+++ home-manager/modules/programs/shell/ssh.nix
@@ -4,11 +4,6 @@
     enable = true;
     addKeysToAgent = "yes";
     matchBlocks = {
-      "kcloud" = {
-        user = "wolfie";
-        hostname = "89.58.16.80";
-        port = 65522;
-      };
       "kbackup" = {
         user = "jonas";
         hostname = "130.255.78.72";
