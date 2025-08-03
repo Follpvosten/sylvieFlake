685ebbddd1ad823b332804a84da76dc08cd07621
Sylvie Hacker <sylvie@karpador.xyz>
2025-08-03 14:28:24 +0200

    use nixos-hardware

--- flake.nix
+++ flake.nix
@@ -3,19 +3,23 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
+  outputs = { nixpkgs, nixos-hardware, home-manager, ... }: {
     nixosConfigurations = {
       godemiche = nixpkgs.lib.nixosSystem { 
         system = "x86_64-linux";
         modules = [
           ./nixos/configuration.nix
 
+          nixos-hardware.nixosModules.common-cpu-amd-zenpower
+          nixos-hardware.nixosModules.common-cpu-amd-pstate
+          nixos-hardware.nixosModules.common-gpu-amd
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
